7c339be0f3ce6caa5d900a609f1c67b798821d9f
BaeGyumin <clouddb2020@gmail.com>
2025-06-11 00:14:51 +0900

    기본위젯구성

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -1,15 +1,123 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _nameState();
+  State<HomePage> createState() => _HomePage();
 }
 
-class _nameState extends State<HomePage> {
+class _HomePage extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage(
+                  'https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=1920&q=80',
+                ),
+              ),
+            ),
+          ),
+          BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
+            child: Container(
+              color: Colors.black.withOpacity(0.1), // 살짝 어둡게(선택)
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Text('소개'),
+                        SizedBox(width: 20),
+                        Text('사용방법'),
+                        SizedBox(width: 20),
+                        Text('홈'),
+                      ],
+                    ),
+                    Text(
+                      '나만의 집중공간',
+                      style: TextStyle(fontSize: 30, height: 0.8),
+                    ),
+                    Row(
+                      children: [
+                        Text('설정'),
+                        Text('설정'),
+                        Text('설정'),
+                        Text('설정'),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Column(
+                children: [
+                  SizedBox(
+                    child: Column(
+                      children: [
+                        Text(
+                          '남은시간',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w100,
+                          ),
+                        ),
+                        Text(
+                          '00:00:00',
+                          style: TextStyle(
+                            fontSize: 50,
+                            fontWeight: FontWeight.w900,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('일시정지'),
+                        SizedBox(width: 30),
+                        Text('그만두기'),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Text('나만의 공간 만들기', style: TextStyle(fontSize: 30)),
+                      ],
+                    ),
+
+                    Row(children: [Text('BLACKPEN SOFT.')]),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mine_space/features/home/home_page.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MineSpace());
 }
 
